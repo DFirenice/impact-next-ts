a854014406c63faef4e8e2f2d470475652c76ca5
DFirenice <firenic.firenice@gmail.com>
2024-09-01 17:41:38 -0400

    (Minor) Indicator's pointer events nullified

--- src/utils/applyFilter.tsx
+++ src/utils/applyFilter.tsx
@@ -1,6 +1,6 @@
 const filter = (query: string, predicate: any[], property: string | undefined = undefined) => {
     const lowerQuery = query.toLowerCase()
-    
+
     const filteredList = predicate.filter(item => {
         if ( property ) return item[property!].toString().toLowerCase().includes(lowerQuery)
         else return item.toString().toLowerCase().includes(lowerQuery)
