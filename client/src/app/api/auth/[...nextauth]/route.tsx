3a9e5489e86de52189664fd34d91b901559e5026
DFirenice <firenic.firenice@gmail.com>
2024-12-23 00:19:41 -0500

    Session, signIn, refresh & access tokens NextAuth fix

--- client/src/app/api/auth/[...nextauth]/route.tsx
+++ client/src/app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,77 @@
-// import NextAuth from "next-auth/next"
-// import { authOptions } from "@/../lib/authOptions"
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import axios, { isAxiosError } from "axios"
 
-// const handler = NextAuth(authOptions)
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                email: { label: 'E-mail', type: 'text' },
+                password: { label: 'Password', type: 'password' }
+            },
+            // Expected credentials to be { email & password }
+            authorize: async (credentials) => {
+                // Authorizing user on backend
+                try {
+                    const { data } = await axios.post(
+                        `http://localhost:8080/api/auth/login`,
+                        { email: credentials?.email, password: credentials?.password }
+                    )
+                    // Returning 'user'
+                    return {
+                        id: data.user._id,
+                        username: data.user.username,
+                        email: data.user.email,
+                        jwt: data.token, // Acess token
+                        refresh: data.refresh // Refresh token
+                    }
+                } catch (err) {
+                    console.error('Error in authorize: ', err)
+                    return null
+                }
+            }
+        })
+    ],
+    callbacks: {
+        // Auth Process:
+        // authorize() (in CredentialsProvider) --> jwt() --> session() --> useSession()
+        async jwt({ token, user }) {
+            const isAccessTokenExpired = Date.now() > (token.accessTokenExpires || 0)
 
-// export { handler as GET, handler as POST }
+            if (user) {
+                token.accessToken = user.jwt
+                token.accessTokenExpires = Date.now() + 15 * 60 * 1000 // 15 mins
+                token.refreshToken = user.refresh
+                token.user = user
+            } else if (isAccessTokenExpired) {
+                try {
+                    // Refreshing both if accessToken expired
+                    const res = await axios.post(
+                        `http://localhost:8080/refresh-token`,
+                        { token: token.refreshToken }
+                    )
+
+                    token.accessToken = res.data.accessToken
+                    token.accessTokenExpires = Date.now() + 15 * 60 * 1000 // 15 mins
+                    token.refreshToken = res.data.refreshToken || token.refreshToken
+                } catch (err) {
+                    if (isAxiosError(err)) {
+                        console.error("Refresh token failed:", err.message)
+                    } else { console.error("Refresh token failed, unexpected error!") }
+                }
+            }
+
+            return token
+        },
+        // Making data available to user's session
+        async session({ session, token }) {
+            session.accessToken = token.accessToken
+            session.user = token.user
+            return session
+        }
+    },
+    session: { strategy: 'jwt' },
+    secret: process.env.NEXTAUTH_SECRET,
+})
+
+export { handler as GET, handler as POST }
