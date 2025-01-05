bdddf494b7b413f1ffe999b04baa692aa75367b1
DFirenice <firenic.firenice@gmail.com>
2025-01-05 18:00:10 -0500

    isUploading state, minor fixes

--- client/src/app/(app)/settings/public/page.tsx
+++ client/src/app/(app)/settings/public/page.tsx
@@ -1,98 +1,18 @@
-'use client'
-
-import authApiClient from '@root/lib/authApiClient'
-import useFModal from '@/hooks/useFModal'
-import { useRef, useState, useEffect } from 'react'
-
 import Heading from '@/components/Heading'
-import Dropbox from '@/components/UI/Dropbox/Dropbox'
-import Btn from '@/components/UI/Btn/Btn'
+import AvatarSection from '@/components/pages/Settings/PublicProfileTab/Avatar'
 
-import type { TDropboxInput } from '@/components/UI/Dropbox/Dropbox.types'
-import AvatarModalCSS from './styles.module.css'
 import css from '../styles.module.css'
 
 const PublicProfileTab = () => {
-    const selectedAvatarRef = useRef<TDropboxInput>(null)
-    const [preview, setPreview] = useState<string>()
-
-    const cleanupCallback = () => { selectedAvatarRef.current = null }
-    const { addFModal, closeFModals, fModalPortal } = useFModal({ cleanupCallback })
-
-    // Avatar store & upload
-    const changeSelectedAvatar = (newAvatar: TDropboxInput) => {
-        selectedAvatarRef.current = newAvatar
-        if (newAvatar) {
-            const objectUrl = URL.createObjectURL(newAvatar as File)
-            setPreview(objectUrl)
-        }
-    }
-
-    // Cleaning up memo from unmounted / replaced URL
-    useEffect(() => {
-        return () => {
-            if (preview) URL.revokeObjectURL(preview)
-        }
-    }, [preview])
-
-    // Avatar
-    const handleUpload = async () => {
-        const selectedAvatar = selectedAvatarRef.current
-        if (!selectedAvatar) {
-            return alert('No file selected!')
-        }
-
-        const formData = new FormData()
-        formData.append('avatar', selectedAvatar as File)
-
-        try {
-            const res = await authApiClient.post(
-                '/upload-avatar',
-                formData,
-                { headers: { 'Content-Type': 'multipart/form-data' } }
-            )
-            console.log(res.data)
-        } catch (err) {
-            console.log('Error uploading avatar: ', err)
-            alert('Error uploading new avatar!') // Replace with toast
-        }
-    }
-
-    // Avatar
-    const handleOpenModal = () => {
-        const AvatarModal = () => (
-            <div className={AvatarModalCSS.modal}>
-                <Dropbox onFileSelect={changeSelectedAvatar} selection="single" />
-                <div className={css.inline_container}>
-                    <Btn func={closeFModals}>Cancel</Btn>
-                    <Btn func={handleUpload} classes="btn-fill">Upload</Btn>
-                </div>
-            </div>
-        )
-
-        addFModal(<AvatarModal />)
-    }
-
     return (
         <>
-            { fModalPortal() }
             <div className={css.settings}>
                 {/* Intro */}
                 <div>
                     <Heading size="large">Your Public Profile</Heading>
                     <p data-font-accent="medium">Manage your public data which is visible to all who can see your profile</p>
                 </div>
-                {/* Avatar */}
-                <div className={css.inline_category}>
-                    <div className={css.category_description}>
-                        <span>Avatar</span>
-                        <span>Change your profile picture</span>
-                    </div>
-                    <div className={css.inline_container}>
-                        <Btn classes="btn-dark btn-disabled btn-pretty ">Remove avatar</Btn>
-                        <Btn classes="btn-dark btn-pretty" func={handleOpenModal}>Change Avatar</Btn>
-                    </div>
-                </div>
+                <AvatarSection />
             </div>
         </>
     )
---
+++ client/src/components/pages/Settings/PublicProfileTab/Avatar.tsx
@@ -0,0 +1,120 @@
+'use client'
+
+import authApiClient from '@root/lib/authApiClient'
+import useFModal from '@/hooks/useFModal'
+import { useRef, useState, useEffect } from 'react'
+
+import Dropbox from '@/components/UI/Dropbox/Dropbox'
+import Btn from '@/components/UI/Btn/Btn'
+import Image from 'next/image'
+
+import type { TDropboxInput } from '@/components/UI/Dropbox/Dropbox.types'
+
+import AvatarModalCSS from '@/app/(app)/settings/public/styles.module.css'
+import css from '@/app/(app)/settings/styles.module.css'
+
+const AvatarSection = () => {
+    const selectedAvatarRef = useRef<TDropboxInput>(null)
+    const [ preview, setPreview ] = useState<string>('')
+
+    const cleanupCallback = () => { selectedAvatarRef.current = null }
+    const { addFModal, closeFModals, fModalPortal } = useFModal({ cleanupCallback })
+
+    // Avatar store & upload
+    const changeSelectedAvatar = (newAvatar: TDropboxInput) => {
+        selectedAvatarRef.current = newAvatar
+        if (newAvatar) {
+            const objectUrl = URL.createObjectURL(newAvatar as File)
+            setPreview(objectUrl)
+        }
+    }
+
+    // Cleaning up memo from unmounted / replaced URL
+    useEffect(() => {
+        if (preview) { handleOpenModal() }
+        return () => {
+            if (preview) { URL.revokeObjectURL(preview) }
+            setPreview('')
+        }
+    }, [ preview ])
+
+    // Avatar upload
+    const handleUpload = async () => {
+        const selectedAvatar = selectedAvatarRef.current
+        if (!selectedAvatar) {
+            return alert('No file selected!')
+        }
+
+        const formData = new FormData()
+        formData.append('avatar', selectedAvatar as File)
+
+        try {
+            const res = await authApiClient.post(
+                '/upload-avatar',
+                formData,
+                { headers: { 'Content-Type': 'multipart/form-data' } }
+            )
+            console.log(res.data) // debug
+
+        } catch (err) {
+            console.log('Error uploading avatar: ', err)
+            alert('Error uploading new avatar!') // Replace with toast
+        } finally { closeFModals() }
+    }
+
+    // Avatar previewer
+    const renderPreview = () => {
+        if (preview) {
+            return <div  className={AvatarModalCSS.avatar_preview}>
+                <Image
+                    alt='Avatar Preview'
+                    src={preview}
+                    fill={true}
+                />
+            </div>
+        }
+
+        return <Dropbox onFileSelect={changeSelectedAvatar} selection="single" />
+    }
+
+    // Adding AvatarModal to fModals
+    const handleOpenModal = () => {
+        const AvatarModal = () => {
+            const [ isUploading, setUploading ] = useState(false)
+            const handleClick = async () => {
+                setUploading(true)
+                await handleUpload().then(() => setUploading(false))
+            }
+            
+            return (
+                <div className={AvatarModalCSS.modal}>
+                    { renderPreview() }
+                    <div className={css.inline_container}>
+                        <Btn func={closeFModals}>Cancel</Btn>
+                        <Btn func={handleClick} classes={`btn-fill ${isUploading && 'btn-disabled'}`}>
+                            { isUploading ? 'Uploading...' : 'Upload' }
+                        </Btn>
+                    </div>
+                </div>
+            )
+        }
+
+        addFModal(<AvatarModal />)
+    }
+
+    return <>
+        { fModalPortal() }
+        <div className={css.inline_category}>
+            <div className={css.category_description}>
+                <span>Avatar</span>
+                <span>Change your profile picture</span>
+            </div>
+            <div className={css.inline_container}>
+                <Btn classes="btn-dark btn-disabled btn-pretty ">Remove avatar</Btn>
+                <Btn classes="btn-dark btn-pretty" func={handleOpenModal}>Change Avatar</Btn>
+            </div>
+        </div>
+    </>
+}
+
+export default AvatarSection
